02c8567ef95bada5d828787f6c296fa85f4ddecd
Til Kaiser <mail@tk154.de>
2024-01-17 13:54:29 +0100

    Add required target parameter for OpenWrt.mk
    Signed-off-by: Til Kaiser <mail@tk154.de>

--- programs/user/common/OpenWrt.mk
+++ programs/user/common/OpenWrt.mk
@@ -1,6 +1,14 @@
-CC := $(OPENWRT_DIR)/staging_dir/toolchain-x86_64_gcc-12.3.0_musl/bin/x86_64-openwrt-linux-musl-gcc-12.3.0
-CFLAGS := -I$(OPENWRT_DIR)/staging_dir/target-x86_64_musl/usr/include
-LDFLAGS := -L$(OPENWRT_DIR)/staging_dir/target-x86_64_musl/usr/lib
+ifndef TARGET
+$(error Missing TARGET)
+endif
 
-STAGING_DIR = $(OPENWRT_DIR)/staging_dir/toolchain-x86_64_gcc-12.3.0_musl:$(OPENWRT_DIR)/staging_dir/target-x86_64_musl/usr
+
+GCC_VERSION := 12.3.0
+
+CC := $(wildcard $(OPENWRT_DIR)/staging_dir/toolchain-$(TARGET)*_gcc-$(GCC_VERSION)_musl/bin/$(TARGET)-openwrt-linux-musl-gcc)
+CXX := $(wildcard $(OPENWRT_DIR)/staging_dir/toolchain-$(TARGET)*_gcc-$(GCC_VERSION)_musl/bin/$(TARGET)-openwrt-linux-musl-g++)
+CPPFLAGS += -I$(wildcard $(OPENWRT_DIR)/staging_dir/target-$(TARGET)*_musl/usr/include)
+LDFLAGS += -L$(wildcard $(OPENWRT_DIR)/staging_dir/target-$(TARGET)*_musl/usr/lib)
+
+STAGING_DIR = $(wildcard $(OPENWRT_DIR)/staging_dir/toolchain-$(TARGET)*_gcc-$(GCC_VERSION)_musl:$(OPENWRT_DIR)/staging_dir/target-$(TARGET)*_musl/usr)
 export STAGING_DIR
